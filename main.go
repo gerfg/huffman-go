9855863d27d0721296c1db18b4553022eb2e2384
geraldofiguer <geraldo.junior@eng.ci.ufpb.br>
2018-04-04 17:30:05 -0300

    all

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
 )
 
 func main() {
@@ -12,18 +13,18 @@ func main() {
 
 	fmt.Print("\n\n\tInitialize Tree\n\n")
 	arrayNodes := initializeNodes(frequency)
-	for idx, vl := range arrayNodes {
-		fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
-	}
+	// for idx, vl := range arrayNodes {
+	// 	fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
+	// }
 
 	fmt.Print("\n\n\tSort arrayNodes\n\n")
 
 	sort.Slice(arrayNodes, func(i, j int) bool {
 		return arrayNodes[i].Freq < arrayNodes[j].Freq
 	})
-	for idx, vl := range arrayNodes {
-		fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
-	}
+	// for idx, vl := range arrayNodes {
+	// 	fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
+	// }
 
 	fmt.Print("\n\n\tHuffman Tree Creation\n\n")
 	tree := generateHuffmanTree(arrayNodes)
@@ -31,7 +32,6 @@ func main() {
 
 	fmt.Print("\n\n\tBits Table\n\n")
 
-	// buffer := Codes{bits: "", i: 0, used: false}
 	cd := make([]Codes, 256)
 	for _, vl := range cd {
 		vl.bits += ""
@@ -39,12 +39,12 @@ func main() {
 		vl.used = false
 	}
 
-	var cds = make(map[uint]string, size)
+	var cds = make(map[uint]byte, size)
 	generateCodes(tree, cds)
 
 	fmt.Println("\n\n Map: \n")
 	for id, vl := range cds {
-		fmt.Printf("-> %d %d - %s \n", string(id), id, vl)
+		fmt.Printf("-> %d - %0"+strconv.Itoa(int(vl))+"b \n", string(id), vl)
 	}
 
 }
--- node.go
+++ node.go
@@ -13,8 +13,6 @@ type Node struct {
 	Dir    *Node
 }
 
-// var codes map[string]string
-
 func (n Node) initializeNode(l uint, f int, e *Node, d *Node) {
 	n = Node{l, f, e, d}
 }
@@ -30,10 +28,10 @@ func initializeNodes(frequency []int) (arrayNodes []Node) {
 
 func generateHuffmanTree(arrayNodes []Node) (tree Node) {
 	for len(arrayNodes) > 1 {
-		fmt.Println(len(arrayNodes))
-		for idx, vl := range arrayNodes {
-			fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
-		}
+		// fmt.Println(len(arrayNodes))
+		// for idx, vl := range arrayNodes {
+		// 	fmt.Printf("ID: %d - Letter: %s - Freq: %d - Esq: %v - Dir: %v\n", idx, string(vl.Letter), vl.Freq, vl.Esq, vl.Dir)
+		// }
 		// arrayNodes[0] + arrayNodes[1]
 		arrayNodes[1] = insertHuffmanTree(arrayNodes[0], arrayNodes[1])
 		arrayNodes = append(arrayNodes[:0], arrayNodes[1:]...)
@@ -49,20 +47,31 @@ func insertHuffmanTree(n1 Node, n2 Node) (n Node) {
 	return n
 }
 
-func generateCodes(tree Node, cds map[uint]string) {
+func generateCodes(tree Node, cds map[uint]byte) {
 
-	var traverse func(n *Node, code uint64, bits byte, cds map[uint]string)
+	var traverse func(n *Node, code uint64, bits byte, cds map[uint]byte)
 
-	traverse = func(n *Node, code uint64, bits byte, cds map[uint]string) {
+	traverse = func(n *Node, code uint64, bits byte, cds map[uint]byte) {
 		if n.Esq == nil || n.Dir == nil {
 			// Leaf
 			fmt.Printf("'%c': %0"+strconv.Itoa(int(bits))+"b  freq: %d\n", n.Letter, code, n.Freq)
-			cds[n.Letter] = strconv.Itoa(int(bits))
 			return
 		}
 		bits++
 		traverse(n.Esq, code<<1, bits, cds)
 		traverse(n.Dir, code<<1+1, bits, cds)
 	}
+
+	var walkTree func(n *Node, code *string, cds map[string]string)
+
+	walkTree = func(n *Node, code *string, cds map[string]string) {
+		if n.Esq == nil || n.Dir == nil {
+			fmt.Printf("'%c' - %s\n", n.Letter, code)
+		}
+		*code += *code + "0"
+	}
+	var code string
+	var cdss = make(map[string]string, 256)
+	walkTree(&tree, &code, cdss)
 	traverse(&tree, 0, 0, cds)
 }
